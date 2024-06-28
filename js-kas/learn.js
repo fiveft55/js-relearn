cbf01a315e063c4026989933769a4d361e30df95
fiveft55 <arpanx980@gmail.com>
2024-06-28 20:02:11 +1000

    updated js

--- js-kas/learn.js
+++ js-kas/learn.js
@@ -2,9 +2,33 @@
 // console.log("Hello world!")
 
 // variables
-var firstName = 'src', secName = 'alt' 
-console.log(firstName, secName)
+// var firstName = 'src', secName = 'alt' 
+// console.log(firstName, secName)
+
+// Const
+// const D_NAME = 'src'
+// console.log(D_NAME)
+
+// Data types
+let favFruit = 'apple'
+let favDrink = "water" // strings
+
+let noOfEnem = 12 // num
+let bigNUM = 1021313131331n // bigint
+let pi = 3.14 // float
+
+let answer = true // boolean
+let favColor; // undefined
+
+// object dt, reference types
+let course = {
+    name: "Js for beginner",
+    hours: 3
+}
 
 
+
+
+console.log(bigNUM)
 
 
