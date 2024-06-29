073f16f6c94fe3df2538b586b852f86fa41a19de
kas-alt <arpanx980@gmail.com>
2024-06-30 02:20:45 +1000

    else-state

--- js-kas/fizbuz.js
+++ js-kas/fizbuz.js
@@ -3,4 +3,5 @@ function fizBuz(num1) {
     if (num1%3 == 0 && num1%5 == 0 ) return 'fizbuz'
     if (num1%3 == 0 && num1%5 != 0 ) return 'fiz'
     if (num1%3 != 0 && num1%5 == 0 ) return 'buz'
+    if (num1%3 != 0 && num1%5 != 0 ) return num1
 }; console.log(fizBuz(numValue))
