b4927685b27901fd3533382e9e83144a083479fa
fiveft55 <arpanx980@gmail.com>
2024-06-29 18:35:29 +1000

    added fizbuz.js

---
+++ js-kas/fizbuz.js
@@ -0,0 +1,6 @@
+let numValue = 15;
+function fizBuz(num1) {
+    if (num1%3 == 0 && num1%5 == 0 ) return 'fizbuz'
+    if (num1%3 == 0 && num1%5 != 0 ) return 'fiz'
+    if (num1%3 != 0 && num1%5 == 0 ) return 'buz'
+}; console.log(fizBuz(numValue))
