e20201793b25671aacc835338c24897f3529ef95
kas-alt <arpanx980@gmail.com>
2024-06-30 21:27:21 +1000

    updated

--- js-kas/learn.js
+++ js-kas/learn.js
@@ -110,10 +110,10 @@
 //     }
 // }
 
-// function getDog(name, bread, age, weight) {
+// function getDog(name, breed, age, weight) {
 //     return {
 //         name,
-//         bread,
+//         breed,
 //         age,
 //         weight,
 //         eat() {
@@ -127,4 +127,142 @@
 
 // const anotherDog = getDog('Marley', 'Chocolate lab', 3, 60)
 // console.log(anotherDog)
+
+// Constructor Functions
+// function Dog(name, breed, age, weight) {
+//     // this = {}
+    
+//     // add properties
+//     this.name = name
+//     this.breed = breed
+//     this.age = age
+//     this.weight = weight
+
+//     this.eat = function() {
+//         console.log(this.name + ': Chomp!')
+//     }
+//     this.bark = function(){
+//         console.log(this.name + ': Woof!')
+//     }
+
+//     // return this;
+// }
+
+// const anotherDog = new Dog('Marley', 'Lab', 3, 60)
+// console.log(anotherDog)
+
+// Objs are dynamic
+// const person = {
+//     name: 'Steve'
+// }
+// // console.log(person)
+// person.favFood = 'chips'
+// person.favDrink = 'fanta'
+// delete person.favFood
+// // console.log(person)
+
+// person.eat = function() {
+//     console.log('start eating')
+// }
+// person.eat()
+
+// Constructor Prop
+// const person = {
+//     name: 'Steve'
+// }
+// console.log( person.constructor )
+
+// let newObj = {} // or u can use the below one, they->same
+// let newObj = new Object();
+
+// new String();
+// new Boolean(); // contructor property demo
+
+// Func() as objects
+// function add(num1, num2) {
+//     return num1 + num2
+// }
+// const n = add
+// console.log(n(2, 2))
+
+// function Programmer(name) {
+//     this.name = name
+//     this.wcode = function () {
+//         console.log("Code in Js")
+//     }
+// }
+// console.log(Programmer.length)
+// console.log(Programmer.constructor)
+
+// const Programmer = new Function('name', `
+//     this.name = name;
+//     this.wcode = function () {console.log('code in js')};
+// `);
+
+// const newProgrammer = new Programmer('Steve')
+// newProgrammer.wcode();
+
+// value vs reference dtypes
+// let a = 10
+// let b = a
+// a = 20
+// console.log(a)
+// console.log(b)
+
+// let a = {value: 20}
+// let b = a
+
+// a.value = 100
+// Object.assign(b,a)
+// console.log(a)
+// console.log(b)
+
+// enum props of an obj
+// let num = [1,2,3,4,5]
+// for (const elem of num) {
+//     console.log(elem)
+// }
+
+// const dog = {
+//     name: 'max',
+//     age: 5,
+//     eyColor: 'blue'
+// }
+// // for (const key in dog) console.log(dog[key])
+
+// const keys = Object.keys(dog) // enum the for...in loop as array
+// // console.log(keys)
+
+// const values = Object.values(dog)
+// // console.log(values)
+
+// const entries = Object.entries(dog)
+// console.log(entries)
+
+// for (const key of Object.keys(dog)) {
+//     console.log(key)
+// }
+
+// Cloning an obj
+// let a = {value: 20}
+// let b = {}
+// Object.assign(b,a)
+// a.value = 100
+
+// console.log(a)
+// console.log(b)
+
+// using spread operator to clone indipendent objs
+// ...var --> Object.assign(b,a)
+
+// let a = {value: 20}
+// let b = { ...a }
+
+// a.value = 10
+// console.log(a)
+// console.log(b)
+
+// String Methods: 2:3:20
+
+
 
